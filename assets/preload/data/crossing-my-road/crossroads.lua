c345fd44a6cab5e93d46773c083ae4c0041bd376
Noobandguestgang <95111164+Noobandguestgang@users.noreply.github.com>
2023-03-16 21:44:14 +0700

    Fixed

--- assets/preload/data/crossing-my-road/crossroads.lua
+++ assets/preload/data/crossing-my-road/crossroads.lua
@@ -6,7 +6,7 @@ function onCreate()
 	makeLuaSprite('stagefront', 'bg/floor', -250, 14);
 	setScrollFactor('stagefront', 1, 1);
 
-	makeAnimatedLuaSprite('guest', 'bg/Guest', 710, 220)
+	makeAnimatedLuaSprite('guest', 'bg/Guest', 695, 139)
     addAnimationByPrefix('guest', 'bop', 'Guest Idle', 24, true)
 	scaleObject('guest', 0.7, 0.7);
 
@@ -14,11 +14,11 @@ function onCreate()
     addAnimationByPrefix('builderman', 'bop', 'Builderman Idle', 24, true)
 	scaleObject('builderman', 0.7, 0.7);
 
-	makeAnimatedLuaSprite('noob', 'bg/Nooblings', 89, 330)
+	makeAnimatedLuaSprite('noob', 'bg/Nooblings', 30, 256)
     addAnimationByPrefix('noob', 'bop', 'Nooblings Idle', 24, true)
 	scaleObject('noob', 0.7, 0.7);
 
-	makeAnimatedLuaSprite('back', 'bg/bggamers', 150, 330)
+	makeAnimatedLuaSprite('back', 'bg/bggamers', 116, 378)
     addAnimationByPrefix('back', 'bop', 'Background Bois (toadver) BGChar', 24, true)
 	setScrollFactor('back', 0.9, 0.9);
 	scaleObject('back', 0.7, 0.7);
