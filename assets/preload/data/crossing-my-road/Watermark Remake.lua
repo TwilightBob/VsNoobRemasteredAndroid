818bd86f44bfb30da812e57fad26c617e7066f38
Noobandguestgang <95111164+Noobandguestgang@users.noreply.github.com>
2023-03-19 17:09:20 +0700

    Update Watermark Remake.lua

--- assets/preload/data/crossing-my-road/Watermark Remake.lua
+++ assets/preload/data/crossing-my-road/Watermark Remake.lua
@@ -1,5 +1,5 @@
 function onCreate()
-                     makeLuaText('watermark', songName .. " | Made By Blazing Biscuits (BrG AKA ME)", 0, 0, 697); -- You can edit the created by Prisma Text just dont change anything else
+                     makeLuaText('watermark', songName .. " | Port Blazing Biscuits (BrG AKA ME)", 0, 0, 697); -- You can edit the created by Prisma Text just dont change anything else
 	makeLuaText('psychtitle', 'PSYCH ENGINE v6.3', 2268, 30, 0);
 
 	--Add Size Font
@@ -7,8 +7,8 @@ function onCreate()
                      setTextSize('watermark', 20);
 
 	--Add Font for texts
-	setTextFont('psychtitle', 'PixelCombo.ttf')
-	setTextFont('watermark', 'songname.ttf')
+	setTextFont('psychtitle', 'vcr.ttf')
+	setTextFont('watermark', 'vcr.ttf')
 
 	--Add Lua
 	addLuaText('psychtitle');
@@ -18,7 +18,7 @@ function onCreate()
 	makeLuaText('healthText', 'Health: ' .. math.floor(getProperty("health") * 50), 0, 1.30, -2.150)
 	addLuaText('healthText')
 	setTextSize('healthText', 24.65);
-	setTextFont('healthText', 'trebucbd.ttf')
+	setTextFont('healthText', 'vcr.ttf')
 end
 
 function onUpdate(elapsed)
